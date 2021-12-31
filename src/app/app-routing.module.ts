b441f582bf95cb805fadfa8bf94567d018bcccdd
Muthineni Shiva Sai <mutshiva@publicisgroupe.net>
2021-12-31 19:58:05 +0530

    included login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { LoginModule } from 'src/modules/login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../modules/login/login.module').then(m => m.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

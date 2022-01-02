12735833ac4066963d8d8ed4dab78880ccc7b28f
Ganapathi Rao <ganeshthota418@gmail.com>
2022-01-02 21:49:57 +0530

    created user profile dropdown

--- src/modules/categories/categories-list/categories-list.component.ts
+++ src/modules/categories/categories-list/categories-list.component.ts
@@ -1,8 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { AppDriveService } from 'src/services/app-drive.service';
 import { AddCategoryComponent } from '../add-category/add-category.component';
 import { ClientMail } from 'src/environments/googleConsole';
+
+@Component({
+  selector: 'delete-confirmation-dialog',
+  template: `
+  <h4>Are you sure you want to delete?</h4>
+
+  <div class="text-end mt-4">
+    <button class="btn me-3 btn-outline-primary" (click)="closeModel()">Cancel</button>
+    <button class="btn btn-primary">Delete</button>
+  </div>
+
+  `,
+})
+export class DeleteConfirmationDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DeleteConfirmationDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {}
+
+  closeModel() {
+    this.dialogRef.close();
+  }
+
+}
+
+
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
@@ -37,7 +63,19 @@ export class CategoriesListComponent implements OnInit {
 
   addCategory(): void {
     this.dialog.open(AddCategoryComponent, {
-      // width: '250px'
+     data: {title :'Add Category',isEdit: false}
+    });
+  }
+
+  editCategory(): void {
+    this.dialog.open(AddCategoryComponent, {
+     data: {title :'Edit Category', category: {},isEdit: true}
+    });
+  }
+
+  deleteCategory() {
+    this.dialog.open(DeleteConfirmationDialog, {
+     
     });
   }
 
--- src/modules/shared/material.module.ts
+++ src/modules/shared/material.module.ts
@@ -5,7 +5,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -14,11 +14,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     CommonModule,
     MatCardModule,
     MatFormFieldModule,
+    MatMenuModule,
     MatDialogModule
   ],
   exports :[
       MatCardModule,
       MatButtonModule,
+      MatMenuModule,
       MatFormFieldModule,
       MatInputModule
   ]

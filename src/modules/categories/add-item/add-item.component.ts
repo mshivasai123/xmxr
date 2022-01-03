22001458041ebfa2a6fe20bc4efc80d14fb16c07
Ganapathi Rao <ganeshthota418@gmail.com>
2022-01-03 13:27:22 +0530

    crated item list design

--- src/modules/categories/add-item/add-item.component.ts
+++ src/modules/categories/add-item/add-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-item',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<AddItemComponent>,
+    @Inject(MAT_DIALOG_DATA) public data:any
+  ) { }
 
   ngOnInit(): void {
   }
--- src/modules/categories/items-list/items-list.component.ts
+++ src/modules/categories/items-list/items-list.component.ts
@@ -1,4 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AddItemComponent } from '../add-item/add-item.component';
+
+@Component({
+  selector: 'delete-confirmation-dialog',
+  template: `
+  <h4>Are you sure you want to delete?</h4>
+
+  <div class="text-end mt-4">
+    <button class="btn me-3 btn-outline-primary" (click)="closeModel()">Cancel</button>
+    <button class="btn btn-primary">Delete</button>
+  </div>
+  `,
+})
+export class DeleteConfirmationDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DeleteConfirmationDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) { }
+
+  closeModel() {
+    this.dialogRef.close();
+  }
+
+}
+
 
 @Component({
   selector: 'app-items-list',
@@ -7,9 +33,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+  addItem(): void {
+    const dialogRef = this.dialog.open(AddItemComponent, {
+      // width: '250px'
+      data: {
+        title: 'Add Item', isEdit: false
+      },
+    });
+    dialogRef.afterClosed().subscribe((dialogResult) => {
+
+    });
+  }
+
+  editItem(): void {
+    this.dialog.open(AddItemComponent, {
+      data: { title: 'Edit Item', item: {}, isEdit: true }
+    });
+  }
+
+  deleteItem() {
+    this.dialog.open(DeleteConfirmationDialog, {
+
+    })
+
   }
 
 }
